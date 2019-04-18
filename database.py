192f1802b738f144514dcdbb5fc2e1168ce99225
Minhaj Hussain <minhaj.hussain@duke.edu>
2019-04-17 21:01:24 -0400

    Implemented User MongoModel Class
    Issue #7

--- database.py
+++ database.py
@@ -1,4 +1,8 @@
-from pymodm import connect, MongoModel, fields
+from pymodm import connect, MongoModel, EmbeddedMongoModel, fields
 
 connect('mongodb+srv://mah148:7C2BeZmfwzWmSgwW@bme547-gxtrh.mongodb.net/'
         'test?retryWrites=true', 'bme547-db')
+
+
+class User(MongoModel):
+    userID = fields.CharField(primary_key=True)
