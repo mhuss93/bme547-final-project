59e02c0ce814657400c88d698cee9429e07b52a0
Minhaj Hussain <minhaj.hussain@duke.edu>
2019-04-17 21:02:51 -0400

    Implemented Image MongoModel Class
    Issue #7

--- database.py
+++ database.py
@@ -6,3 +6,12 @@ connect('mongodb+srv://mah148:7C2BeZmfwzWmSgwW@bme547-gxtrh.mongodb.net/'
 
 class User(MongoModel):
     userID = fields.CharField(primary_key=True)
+
+
+class Image(MongoModel):
+    name = fields.CharField(primary_key=True)
+    filename = fields.CharField()
+    extension = fields.CharField()
+    image = fields.CharField()
+    uploadedAt = fields.DateTimeField()
+    user = fields.ReferenceField(User)
