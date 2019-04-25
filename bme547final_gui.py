5da4cccb6d60c2eb62e086febd5260fec68b03f6
Katrina Barth <katrina.barth@duke.edu>
2019-04-25 16:36:15 -0400

    Redesign processed image window

--- bme547final_gui.py
+++ bme547final_gui.py
@@ -81,59 +81,72 @@ def main_window():
 def window2():
     window2 = Toplevel()
     window2.title("Processed Image")
-    # Button to open window displaying original and processed image
-    compare_btn = ttk.Button(window2, text='Compare to Original')
-    compare_btn.grid(column=3, row=1, columnspan=2, pady=5)
-    # Button to open window displaying histogram of colors
-    histogram_btn = ttk.Button(window2,
-                               text='Histogram of Color Intensities')
-    histogram_btn.grid(column=3, row=2, columnspan=2, pady=5)
+    # Frame and Label for Original Image
+    img1_label = ttk.Label(window2, text="Original Image", font='Arial 10 bold')
+    img1_label.grid(column=0, row=0, columnspan=4, pady=5)
+    img1_frm = ttk.Frame(window2, borderwidth=1, relief=GROOVE,
+                        width=380, height=380)
+    img1_frm.grid(column=0, row=1, columnspan=4, rowspan=2, pady=5, padx=5)
+    img1_frm.grid_propagate(0)
+    # Frame and Label for processed Image
+    img2_label = ttk.Label(window2, text="Processed Image", font='Arial 10 bold')
+    img2_label.grid(column=4, row=0, pady=5)
+    img2_frm = ttk.Frame(window2, borderwidth=1, relief=GROOVE,
+                        width=380, height=380)
+    img2_frm.grid(column=4, row=1, rowspan=2, pady=5, padx=5)
+    img2_frm.grid_propagate(0)
+    # Frame for metadata
+    data_frm = ttk.Frame(window2, borderwidth=1, relief=GROOVE,
+                        width=200, height=95)
+    data_frm.grid(column=5, row=1, columnspan=2, pady=5, padx=5, sticky=N)
+    data_frm.grid_propagate(0)
     # Metadata
-    timestamp_lbl = ttk.Label(window2, text="Time of Upload:")
-    timestamp_lbl.grid(column=3, row=3, pady=5, columnspan=2, sticky=W)
-    proctime_lbl = ttk.Label(window2, text="Time for Processing:")
-    proctime_lbl.grid(column=3, row=4, pady=5, columnspan=2, sticky=W)
-    size_lbl = ttk.Label(window2, text="Image Size:")
-    size_lbl.grid(column=3, row=5, pady=5, columnspan=2, sticky=W)
+    timestamp_lbl = ttk.Label(data_frm, text="Time of Upload:")
+    timestamp_lbl.grid(column=0, row=0, pady=5, sticky=W)
+    proctime_lbl = ttk.Label(data_frm, text="Time for Processing:")
+    proctime_lbl.grid(column=0, row=1, pady=5, sticky=W)
+    size_lbl = ttk.Label(data_frm, text="Image Size:")
+    size_lbl.grid(column=0, row=2, pady=5, sticky=W)
+    # Button to open window displaying histogram of colors
+    window2.grid_rowconfigure(2, weight=1)
+    histo_btn = ttk.Button(window2,
+                               text='Show Color Histograms')
+    histo_btn.grid(column=5, row=2, pady=10, columnspan=2, sticky=N)
     # Choose the save file type, with JPEG as default
     file_type = StringVar()
     file_type_lbl = ttk.Label(window2,
                               text="Select save file type:")
-    file_type_lbl.grid(column=0, row=7, sticky=W, pady=5)
+    file_type_lbl.grid(column=0, row=3, sticky=E, pady=5, padx=5)
     jpg_box = ttk.Radiobutton(window2, text='JPEG',
                               variable=file_type, value='.jpg')
-    jpg_box.grid(column=1, row=7, padx=15, sticky=W)
+    jpg_box.grid(column=1, row=3, padx=15, sticky=W)
     png_box = ttk.Radiobutton(window2, text='PNG',
                               variable=file_type, value='.png')
-    png_box.grid(column=2, row=7, padx=15, sticky=W)
+    png_box.grid(column=2, row=3, padx=15, sticky=W)
     tiff_box = ttk.Radiobutton(window2, text='TIFF',
                                variable=file_type, value='.tiff')
-    tiff_box.grid(column=3, row=7, padx=15, sticky=W)
+    tiff_box.grid(column=3, row=3, padx=15, sticky=W)
     file_type.set('.jpg')
     # Choose where to save the processed image
-    save_file_lbl = ttk.Label(window2, text="Select save location:")
-    save_file_lbl.grid(column=0, row=8, sticky=E, pady=5)
+    save_file_lbl = ttk.Label(window2, text="Save processed image as:")
+    save_file_lbl.grid(column=0, row=4, sticky=E, pady=5, padx=5)
     save_file_adrs = StringVar()
     save_file_box = ttk.Entry(window2, textvariable=save_file_adrs)
-    save_file_box.grid(column=1, row=8, columnspan=2, padx=5)
+    save_file_box.grid(column=1, row=4, columnspan=4, padx=5)
+    save_file_box.config(width=105)
 
     def save_file():
-        save_file_adrs = filedialog.askdirectory()
+        save_file_adrs = filedialog.askopenfilename()
         save_file_box.insert(0, save_file_adrs)
+        window2.lift()
         pass
     browse_btn = ttk.Button(window2, text='Browse', command=save_file)
-    browse_btn.grid(column=3, row=8, sticky=W)
+    browse_btn.grid(column=5, row=4, sticky=W)
     # Save image button
     save_btn = ttk.Button(window2, text="Save Processed Image")
-    save_btn.grid(column=4, row=8, pady=5, padx=5)
+    save_btn.grid(column=6, row=4, pady=5, padx=5)
     window2.mainloop()
     return
-
-
-def compare_window():
-    window3 = tk.Toplevel(window2)
-    window3.title("Image Comparison")
-    window3.geometry('400x300+100+100')
 
 
 def histogram_window():
