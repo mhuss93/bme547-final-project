b675b9a8e0af6542cb1f3061a387ec3c2c1b8d66
IakovRach <iakov.rachinskiy@duke.edu>
2019-04-25 12:53:11 -0400

    fixed pep 8 for equalize

--- equalize_img.py
+++ equalize_img.py
@@ -1,7 +1,7 @@
-from matplotlib.pyplot import imread, imshow, show, subplot, title, get_cmap, hist
+import matplotlib.pyplot as plt
 from skimage.exposure import equalize_hist
 import numpy as np
-import matplotlib.pyplot as plt
+
 
 # histogram equalization
 def equalize_img(img_loc):
@@ -17,16 +17,23 @@ def equalize_img(img_loc):
     eq - 2D array of equalized image
     """
     img = imread(img_loc)
-    eq = np.asarray(equalize_hist(img)*255, dtype= 'uint8')
+    eq = np.asarray(equalize_hist(img)*255, dtype='uint8')
     return img, eq
 
 
 if __name__ == "__main__":
-
     img, eq_img = equalize_img('image_0001.jpg')
     plt.imsave('equalized_image.jpg', eq_img)
-    subplot(221); imshow(img, cmap= get_cmap('gray')); title('Original')
-    subplot(222); hist(img.flatten(), 256, range=(0,256)); title('Histogram of original')
-    subplot(223); imshow(eq_img, cmap= get_cmap('gray')); title('Equalized')
-    subplot(224); hist(eq_img.flatten(), 256, range=(0,256)); title('Histogram of equalized')
+    subplot(221)
+    imshow(img, cmap=get_cmap('gray'))
+    title('Original')
+    subplot(222)
+    hist(img.flatten(), 256, range=(0, 256))
+    title('Histogram of original')
+    subplot(223)
+    imshow(eq_img, cmap=get_cmap('gray'))
+    title('Equalized')
+    subplot(224)
+    hist(eq_img.flatten(), 256, range=(0, 256))
+    title('Histogram of equalized')
     show()
