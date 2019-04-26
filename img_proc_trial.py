e8dd1b8da54b219941ca1cd6d1ab5033b3dfaff5
IakovRach <iakov.rachinskiy@duke.edu>
2019-04-26 11:55:52 -0400

    fixed pep-8

--- img_proc_trial.py
+++ img_proc_trial.py
@@ -5,8 +5,9 @@ from img_proc import reverse_img
 from img_proc import RGB
 from img_proc import plot_rgb
 
+
 if __name__ == "__main__":
     img_loc = 'image_0008.jpg'
     img, equalized = equalize_img(img_loc)
-    r,g,b = RGB(equalized)
+    r, g, b = RGB(equalized)
     plot_rgb(r, g, b)
