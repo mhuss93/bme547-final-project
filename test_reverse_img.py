d87846d7bd27a9ffd68f988924cd78ae713b41cf
IakovRach <iakov.rachinskiy@duke.edu>
2019-04-29 05:49:59 -0400

    fixng missing module erro ron travis

--- test_reverse_img.py
+++ test_reverse_img.py
@@ -1,13 +1,5 @@
 import pytest
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 import numpy as np
-from matplotlib.pyplot import imread, imshow, show, subplot, title
-from matplotlib.pyplot import get_cmap, hist
-import base64
-import struct
-import io
-import cv2
 
 
 # test the encode and decode funcs
@@ -32,21 +24,3 @@ def test_encode_decode(img, expected):
     print(reverse)
     summa = np.sum(reverse)
     assert summa == expected
-
-
-# visual testing
-if __name__ == "__main__":
-    # test the encode and decode funcs
-    black = np.zeros((100, 100, 3))
-    sum_black = np.sum(black)
-    white = np.ones((100, 100, 3))*255
-    sum_white = np.sum(white)
-    # visual testing
-    black = np.zeros((100, 100, 3))
-    white = np.ones((100, 100, 3))
-    reverse = reverse_img(white)*255
-    print(reverse)
-    imshow(white, cmap=get_cmap('gray'))
-    show()
-    imshow(reverse, cmap=get_cmap('gray'))
-    show()
