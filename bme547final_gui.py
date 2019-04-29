8d4f3a82eb95bc39487b0cc03153b37526f41775
Katrina Barth <katrina.barth@duke.edu>
2019-04-29 06:42:33 -0400

    Dropdown Menu to Show Images from Database

--- bme547final_gui.py
+++ bme547final_gui.py
@@ -21,7 +21,10 @@ import requests
 from flask import Flask, jsonify, request
 import os
 
+# Global variables
 url = 'http://127.0.0.1:5000'
+multi_file = 0
+userID = ''
 
 # Mocks for testing
 img_path = "Zoey.jpg"
@@ -29,8 +32,13 @@ with open(img_path, "rb") as img_file:
     b64_bytes = base64.b64encode(img_file.read())
 b64_string_proc = str(b64_bytes, encoding='utf-8')
 mockDB = {
+
           }
-multi_file = 0
+mockMeta = {
+            "user_id": "kb",
+            "filename": ["MentorLunchReceipt_3_27_2019", "MiddleSupport"],
+            "extension": ["jpg", "jpg"],
+            }
 
 
 def main_window():
@@ -44,6 +52,7 @@ def main_window():
 
     # Asking for user ID
     def post_userID():
+        global userID
         userID = userID_box.get()
         mockDB["user_id"] = userID
         requests.post(url + "/api/register_user", json=mockDB)
@@ -56,7 +65,6 @@ def main_window():
     userID_box.grid(column=1, row=1, padx=5, pady=5, columnspan=2)
     userID_btn = ttk.Button(root, text="Enter ID", command=post_userID)
     userID_btn.grid(column=3, row=1, padx=5, pady=5)
-
 
     # Labels, boxes, and buttons for image file selection
     def ask_file():
@@ -80,8 +88,8 @@ def main_window():
         global multi_file
         multi_file = 1
         pass
-    open_folder_lbl = ttk.Label(root,
-                              text="OR select folder of images for processing:")
+    lbl = "OR select folder of images for processing:"
+    open_folder_lbl = ttk.Label(root, text=lbl)
     open_folder_lbl.grid(column=0, row=3, pady=5, padx=10)
     folder_adrs = StringVar()
     open_folder_box = ttk.Entry(root, textvariable=folder_adrs)
@@ -117,10 +125,11 @@ def main_window():
     def get_file_name(img_path):
         file_adrs_str = img_path
         file_name = ""
+        fas = file_adrs_str
         for x in range(len(file_adrs_str)):
             if file_adrs_str[x] == '.':
                 for n in range(len(file_adrs_str)):
-                    if file_adrs_str[x-n-1] != '/' and file_adrs_str[x-n-1] != '\\':
+                    if fas[x-n-1] != '/' and fas[x-n-1] != '\\':
                         file_name = file_name + file_adrs_str[x-n-1]
                     else:
                         break
@@ -141,13 +150,11 @@ def main_window():
         if multi_file == 0:
             proc = proc_choice.get()
             img_path = open_file_box.get()
-            userID = userID_box.get()
             with open(img_path, "rb") as img_file:
                 b64_bytes = base64.b64encode(img_file.read())
             b64_string = str(b64_bytes, encoding='utf-8')
             file_name = get_file_name(img_path)
             file_type = get_file_type(img_path)
-            # mockDB["user_id"] = userID
             mockDB["filename"] = file_name
             mockDB["extension"] = file_type
             mockDB["method"] = proc
@@ -164,24 +171,16 @@ def main_window():
                     files.append(os.path.join(r, file))
             for f in files:
                 proc = proc_choice.get()
-                print(type(proc))
                 img_path = f
-                print(type(img_path))
-                userID = userID_box.get()
-                print(type(userID))
                 with open(img_path, "rb") as img_file:
                     b64_bytes = base64.b64encode(img_file.read())
                 b64_string = str(b64_bytes, encoding='utf-8')
                 file_name = get_file_name(img_path)
-                print(type(file_name))
                 file_type = get_file_type(img_path)
-                print(type(file_type))
-                # mockDB["user_id"] = userID
                 mockDB["filename"] = file_name
                 mockDB["extension"] = file_type
                 mockDB["method"] = proc
                 mockDB["image"] = b64_string
-                print(mockDB)
                 # mockDB["procssed_image"] = b64_string_proc
                 post_adrs = url + "/api/upload_user_image"
                 requests.post(post_adrs, json=mockDB)
@@ -192,45 +191,84 @@ def main_window():
     root.mainloop()
     return
 
+
 def window2(b64_string_proc):
     window2 = Toplevel()
     window2.title("Processed Image Viewer")
+    # In-window global variables
+    img1_array = np.empty([2, 2])
+    # List of file names and extensions
+    file_names = mockMeta["filename"]
+    extensions = mockMeta["extension"]
+    # Drop down menu to select file to view
+    drop_lbl = ttk.Label(window2, text='Select Image to View:')
+    drop_lbl.grid(column=2, row=0, columnspan=2, pady=5, sticky=E)
+    variable = StringVar(window2)
+    variable.set(file_names[0])
+    drop_menu = OptionMenu(window2, variable, *file_names)
+    drop_menu.grid(column=4, row=0, pady=5, padx=10)
     # Frame and Label for Original Image
     img1_lbl = ttk.Label(window2, text="Original Image",
                          font='Arial 10 bold')
-    img1_lbl.grid(column=0, row=0, columnspan=4, pady=5)
+    img1_lbl.grid(column=0, row=1, columnspan=4, pady=5)
     img1_frm = ttk.Frame(window2, borderwidth=1,
                          width=380, height=380)
-    img1_frm.grid(column=0, row=1, columnspan=4, rowspan=2, pady=5,
+    img1_frm.grid(column=0, row=2, columnspan=4, rowspan=2, pady=5,
                   padx=5, ipady=5)
     img1_frm.grid_propagate(0)
-    def get_up_img():
-        r = requests.post(url + "/api/get_uploaded_image", json=mockDB)
+
+    def get_up_img(dic):
+        r = requests.post(url + "/api/get_uploaded_image",
+                          json=dic)
         img_dict = r.json()
         b64_string = img_dict['image']
         img_bytes = base64.b64decode(b64_string)
         img_buf = io.BytesIO(img_bytes)
-        img1_array = mpimg.imread(img_buf, format='JPG')
+        img1_array = mpimg.imread(img_buf, format=dic['extension'])
         return img1_array
-    img1_array = get_up_img()
+    img1_array = get_up_img(mockDB)
     img1_obj = Image.fromarray(img1_array)
     size = (375, 375)
     img1_obj.thumbnail(size)
     img1 = ImageTk.PhotoImage(img1_obj)
     img1_space = ttk.Label(img1_frm, image=img1)
     img1_space.grid(column=0, row=0)
+
+    # Refresh button
+    def refresh_img():
+        for i in range(len(file_names)):
+            if file_names[i] == variable.get():
+                file_name = file_names[i]
+                extension = extensions[i]
+        dic = {
+               "user_id": userID,
+               "filename": file_name,
+               "extension": extension,
+                }
+        img1_array = get_up_img(dic)
+        img1_obj = Image.fromarray(img1_array)
+        size = (375, 375)
+        img1_obj.thumbnail(size)
+        img1 = ImageTk.PhotoImage(img1_obj)
+        img1_space.configure(image=img1)
+        img1_space.image = img1
+        pass
+    refresh_btn = ttk.Button(window2, text='Refresh Image',
+                             command=refresh_img)
+    refresh_btn.grid(column=5, row=0, pady=5, sticky=W)
     # Frame and Label for Processed Image
     img2_lbl = ttk.Label(window2, text="Processed Image",
                          font='Arial 10 bold')
-    img2_lbl.grid(column=4, row=0, pady=5)
+    img2_lbl.grid(column=4, row=1, pady=5)
     img2_frm = ttk.Frame(window2, borderwidth=1,
                          width=380, height=380)
-    img2_frm.grid(column=4, row=1, rowspan=2, pady=5, padx=5, ipady=5)
+    img2_frm.grid(column=4, row=2, rowspan=2, pady=5, padx=5, ipady=5)
     img2_frm.grid_propagate(0)
     img_bytes = base64.b64decode(b64_string_proc)
     img_buf = io.BytesIO(img_bytes)
     img2_array = mpimg.imread(img_buf, format='JPG')
     img2_obj = Image.fromarray(img2_array)
+    size = (375, 375)
     img2_obj.thumbnail(size)
     img2 = ImageTk.PhotoImage(img2_obj)
     img2_space = ttk.Label(img2_frm, image=img2)
@@ -238,7 +276,7 @@ def window2(b64_string_proc):
     # Frame for metadata
     data_frm = ttk.Frame(window2, borderwidth=1, relief=GROOVE,
                          width=200, height=95)
-    data_frm.grid(column=5, row=1, columnspan=2, pady=5, padx=5, sticky=N)
+    data_frm.grid(column=5, row=2, columnspan=2, pady=5, padx=5, sticky=N)
     data_frm.grid_propagate(0)
     timestamp_lbl = ttk.Label(data_frm, text="Time of Upload:")
     timestamp_lbl.grid(column=0, row=0, pady=5, sticky=W)
@@ -247,25 +285,25 @@ def window2(b64_string_proc):
     size_lbl = ttk.Label(data_frm, text="Image Size:")
     size_lbl.grid(column=0, row=2, pady=5, sticky=W)
     # Button to open histogram window
-    window2.grid_rowconfigure(2, weight=1)
+    window2.grid_rowconfigure(3, weight=1)
     histo_btn = ttk.Button(window2,
                            text='Show Color Histograms',
                            command=lambda: plt_histo(img1_array, img2_array))
-    histo_btn.grid(column=5, row=2, pady=10, columnspan=2, sticky=N)
+    histo_btn.grid(column=5, row=3, pady=10, columnspan=2, sticky=N)
     # Choose the save file type, with JPEG as default
     file_type = StringVar()
     file_type_lbl = ttk.Label(window2,
                               text="Select save file type:")
-    file_type_lbl.grid(column=0, row=3, sticky=E, pady=5, padx=5)
+    file_type_lbl.grid(column=0, row=4, sticky=E, pady=5, padx=5)
     jpg_box = ttk.Radiobutton(window2, text='JPEG',
                               variable=file_type, value='.jpg')
-    jpg_box.grid(column=1, row=3, padx=15, sticky=W)
+    jpg_box.grid(column=1, row=4, padx=15, sticky=W)
     png_box = ttk.Radiobutton(window2, text='PNG',
                               variable=file_type, value='.png')
-    png_box.grid(column=2, row=3, padx=15, sticky=W)
+    png_box.grid(column=2, row=4, padx=15, sticky=W)
     tiff_box = ttk.Radiobutton(window2, text='TIFF',
                                variable=file_type, value='.tiff')
-    tiff_box.grid(column=3, row=3, padx=15, sticky=W)
+    tiff_box.grid(column=3, row=4, padx=15, sticky=W)
     file_type.set('.jpg')
 
     # Choosing a save location for the processed image
@@ -278,23 +316,23 @@ def window2(b64_string_proc):
     # Saving the file
     def save_file():
         file_path = save_file_adrs.get() + file_type.get()
-        img1_obj.save(file_path)
+        img2_obj.save(file_path)
         pass
     save_file_lbl = ttk.Label(window2, text="Save processed image as:")
-    save_file_lbl.grid(column=0, row=4, sticky=E, pady=5, padx=5)
+    save_file_lbl.grid(column=0, row=5, sticky=E, pady=5, padx=5)
     save_file_adrs = StringVar()
     save_file_box = ttk.Entry(window2, textvariable=save_file_adrs)
-    save_file_box.grid(column=1, row=4, columnspan=4, padx=5)
+    save_file_box.grid(column=1, row=5, columnspan=4, padx=5)
     save_file_box.config(width=105)
     browse_btn = ttk.Button(window2, text='Browse', command=ask_file)
-    browse_btn.grid(column=5, row=4, sticky=W)
+    browse_btn.grid(column=5, row=5, sticky=W)
     save_btn = ttk.Button(window2, text="Save Processed Image",
                           command=save_file)
-    save_btn.grid(column=6, row=4, pady=5, padx=5)
+    save_btn.grid(column=6, row=5, pady=5, padx=5)
     # Close window button
     close_btn = ttk.Button(window2, text="Close Processed Image Viewer",
                            command=window2.destroy)
-    close_btn.grid(column=0, row=5, columnspan=7, pady=10)
+    close_btn.grid(column=0, row=6, columnspan=7, pady=10)
     window2.mainloop()
     return
 
@@ -350,7 +388,6 @@ def plt_histo(img1_array, img2_array):
     plt.subplots_adjust(top=0.88, right=0.93)
     plt.show()
     return
-
 
 
 if __name__ == '__main__':
